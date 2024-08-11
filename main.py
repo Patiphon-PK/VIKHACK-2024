416b0e6a1d939f84e8c9dbc5803aed54deeb2de8
SVH3M <Sv.visut9@gmail.com>
2024-08-11 16:54:11 +1000

    openai chat

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hellow world")
+from openai import OpenAI
+client = OpenAI()
+
+while (True):
+    user_input = input("Enter prompt: ")
+    if user_input == "exit":
+        print("exiting")
+        break
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", 
+            "content": "You're my math tutor"},
+            {"role": "user", 
+            "content": user_input}
+        ]
+    )
+    print(completion.choices[0].message)
