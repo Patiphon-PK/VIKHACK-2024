ce68959dc2ee7791ff2c161a29f1eee7d6919a5e
Frank40790 <143776004+Frank40790@users.noreply.github.com>
2024-08-16 00:25:40 +1000

    Adding exception handling
    - json decode error in api_server.py

--- api_server.py
+++ api_server.py
@@ -1,10 +1,7 @@
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import StreamingResponse
-from typing import List
 from io import StringIO
-import io
 import json
-from datetime import datetime
 import base64
 
 from backend import Bot
@@ -14,22 +11,27 @@ app = FastAPI()
 
 @app.post("/image/")
 async def image(request: Request):
+    try:
         body = await request.json()
-        base64_image = body.get('image')
-        
-        if not base64_image:
-            raise HTTPException(status_code=400, detail="No image data found")
-        
-        
-        bot = Bot()
-        bot_response = bot.ping_ai(base64_image=base64_image)
-        date_json = bot_response["choices"][0]["message"]["content"]
-        formatted_date = json.dumps(json.loads(date_json), indent=4, sort_keys=True)
-        ics_content = json_to_ics(formatted_date)
-        ics_file = StringIO(ics_content)
+    except json.JSONDecodeError:
+        raise HTTPException(status_code=400, detail="Invalid JSON format")
+    
+    base64_image = body.get('image')
+    
+    if not base64_image:
+        raise HTTPException(status_code=400, detail="No image data found")
+    
+    bot = Bot()
+    bot_response = bot.ping_ai(base64_image=base64_image)
+    date_json = bot_response["choices"][0]["message"]["content"]
+    formatted_date = json.dumps(json.loads(date_json), indent=4, sort_keys=True)
+    ics_content = json_to_ics(formatted_date)
+    ics_file = StringIO(ics_content)
 
-        return StreamingResponse(
-            ics_file,
-            media_type="text/calendar",
-            headers={"Content-Disposition": "attachment; filename=event.ics"}
-        )
+    return StreamingResponse(
+        ics_file,
+        media_type="text/calendar",
+        headers={"Content-Disposition": "attachment; filename=event.ics"}
+    )
+    
+
